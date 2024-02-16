bc6f6ed1cedefed6535ac4922db6ba48df0f6356
DuarteDc <al221910409@gmail.com>
2024-02-16 13:39:08 -0600

    App v1 production

--- app/infrastructure/views/index.php
+++ app/infrastructure/views/index.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
   <script src="http://localhost:8097"></script>
   <title>Vite + React + TS</title>
-  <script type="module" crossorigin src="/assets/index-jQM8V1D0.js"></script>
+  <script type="module" crossorigin src="/assets/index-hpHUH-KC.js"></script>
   <link rel="stylesheet" crossorigin href="/assets/index-MYFj73kf.css">
 </head>
 

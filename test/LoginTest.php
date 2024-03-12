61eecb7dafec8b1752b42660214acaf89187f0b9
DuarteDc <al221910409@gmail.com>
2024-03-11 18:08:11 -0600

    first test

---
+++ test/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Test;
+
+use App\application\authentication\AuthenticationUseCase;
+use App\domain\user\User;
+use App\infrastructure\controllers\AuthenticationController;
+use App\infrastructure\repositories\user\UserRepository;
+use PHPUnit\Framework\TestCase;
+use App\infrastructure\database\Database;
+
+final class LoginTest extends TestCase
+{
+
+
+    public function testLoginUSer() 
+    {
+
+        Database::getInstance()->connection();
+        $userRepository = new UserRepository(new User);
+        $authenticationUseCase = new AuthenticationUseCase($userRepository);
+        // $authenticationController = new AuthenticationController($authenticationUseCase);
+
+        $user = $authenticationUseCase->signin('Eduardo Duarte', '7291073097');
+
+        $this->assertSame('Eduardo Duarte', $user->user->userName);
+
+    }
+}

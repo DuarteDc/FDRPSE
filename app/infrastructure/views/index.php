f43f4aca578772187dab7b485b167fa30bad8606
DuarteDc <al221910409@gmail.com>
2024-04-22 14:51:24 -0600

    Fix: send questions and clear logs

--- app/infrastructure/views/index.php
+++ app/infrastructure/views/index.php
@@ -9,7 +9,7 @@
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
    <title>Cuestionario de análisis de riesgos</title>
-   <script type="module" crossorigin src="/cuestionario/public/assets/index-CgulTY2t.js"></script>
+   <script type="module" crossorigin src="/cuestionario/public/assets/index-BG05uYkY.js"></script>
    <link rel="stylesheet" crossorigin href="/cuestionario/public/assets/index-BP3V76st.css">
  </head>
 

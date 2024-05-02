ca955c7be62613ffe937ede74862172e3114e087
DuarteDc <al221910409@gmail.com>
2024-05-02 10:26:06 -0600

    v2.1

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
-FROM php:8.3
+FROM php:8.2-fpm
 
-RUN apt-get update -y && \
-    apt-get install -y openssl zip unzip git
+RUN apt-get update && apt-get install -y \
+		libfreetype-dev \
+		libjpeg62-turbo-dev \
+		libpng-dev 
 
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-RUN docker-php-ext-install pdo pdo_mysql
 
-COPY . ./app
-WORKDIR /app
+WORKDIR /var/wwww/html/
+COPY . .
 
 ENV COMPOSER_ALLOW_SUPERUSER=1
 RUN composer install
 RUN composer dump-autoload
 
 EXPOSE 8001
+
+CMD [ "php", "./public/index.php" ]
--- app/infrastructure/views/index.php
+++ app/infrastructure/views/index.php
@@ -1,21 +1,20 @@
 <!doctype html>
- <html lang="es">
+<html lang="es">
 
- <head>
-   <meta charset="UTF-8" />
-   <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-   <title>Cuestionario de análisis de riesgos</title>
-   <script type="module" crossorigin src="/cuestionario/public/assets/index-B0MvOw69.js"></script>
-   <link rel="stylesheet" crossorigin href="/cuestionario/public/assets/index-Du5HgzU2.css">
- </head>
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/cuestionario/public/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <title>Cuestionario de análisis de riesgos</title>
+  <script type="module" crossorigin src="/cuestionario/public/assets/index-B0MvOw69.js"></script>
+  <link rel="stylesheet" crossorigin href="/cuestionario/public/assets/index-Du5HgzU2.css">
+</head>
 
- <body>
-   <div id="root"></div>
- </body>
+<body>
+  <div id="root"></div>
+</body>
 
- </html>
-
+</html>
